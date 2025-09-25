4a522c19fdfc25a8c36b985fbd29def2f582cb12
breezyfc <pscott1097@gmail.com>
2025-09-25 14:37:35 -0400

    Add files via upload

--- Scott_Payton_Lab5.py
+++ Scott_Payton_Lab5.py
@@ -3,17 +3,26 @@ import os
 import sys
 
 API_URL = os.getenv ("API_URL", "http://dragonball-api.com/api/characters")
-response = requests.get(API_URL)
 
-if response.status_code == 200:
-    data = response.json()  # Parse JSON response
-    print(data)
-else:
-    print("Error:", response.status_code)
+def fetch_characters():
+    """Fetch the characters from the API."""
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Failed to fetch characters.")
+        sys.exit(1)
+
+def search_by_name(data, name):
+    """Search For Character By Name"""
+    results = [char for char in data if name.lower() in char['name'].lower()]
+    return results
 
 
 def main():
     print("Welcome to the DragonBall Character Database! \n Here youa are able to do many things, elts get started!")
+
+
     
 
 if __name__ == "__main__":
